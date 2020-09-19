ecab5fa493f983cba01dfe53cbda819260ee8d36
gaurangi99 <imgaurangi.99@gmail.com>
2020-09-19 20:11:07 +0530

    added files in folders, dropdown function

--- RDIP/Codes/main.js
+++ RDIP/Codes/main.js
@@ -34,8 +34,8 @@ var engCorpus = [['John ate an apple before afternoon',
     'I bought a book yesterday that I told her',
     'yesterday I bought a book that I told her',]];
 
-    /*Hindi Corpus array*/
-    var hinCorpus = [['राम और श्याम बाजार गयें',
+/*Hindi Corpus array*/
+var hinCorpus = [['राम और श्याम बाजार गयें',
     'राम और श्याम गयें बाजार',
     'बाजार गयें राम और श्याम',
     'गयें बाजार राम और श्याम',],
@@ -81,3 +81,199 @@ var engCorpus = [['John ate an apple before afternoon',
     'वहाँ है बड़ी सी एक किताब',
     'है वहाँ एक बड़ी सी किताब',
     'है वहाँ बड़ी सी एक किताब',]];
+
+var heading = document.getElementById('subject');
+var desc1 = document.getElementById('desc1');
+var desc2 = document.getElementById('desc2');
+var select = document.getElementById('select');
+var selDesc = document.getElementById('selDesc');
+var desc3 = document.getElementById('desc3');
+var desc4 = document.getElementById('desc4');
+var sentence = document.getElementById('sentence');
+var reform = document.getElementById('reformButton');
+var correctness = document.getElementById('correctness');
+var result = document.getElementById('resultButton');
+var correctAns = document.getElementById('correctAns');
+var j, btnCount, r;
+var finalSentence = '';
+var clickCount = 0;
+var x;
+var ans = '';
+function intro() {
+    heading.innerHTML = 'Introduction';
+    desc1.innerHTML ='A sentence can become more complex, if more than one verb is present or by joining two sentences or words using conjunctions or by some other methods.';
+    desc2.innerHTML ='<br>In this experiment also, you will make more difficult sentences using the given words.';
+    /* desc3.innerHTML = '';
+    select.innerHTML = '';
+    selDesc.innerHTML = ''; */
+    desc2.style.textAlign = 'left';
+    /* desc4.innerHTML = ''; */
+    desc4.style.textAlign = 'left';
+    /* sentence.innerHTML = '';
+    reform.innerHTML = '';
+    correctness.innerHTML = '';
+    result.innerHTML = '';
+    ans = '';
+    correctAns.innerHTML = ''; */
+}
+function theory() {
+    heading.innerHTML = 'Theory';
+    desc.innerHTML ='<u><b>Clause</b></U><br>A clause typically contains a subject noun phrase and a finite verb. Some languages allow subjects to be omitted. There are two types of subclauses:<ol><li>independent clause</li><li>subordinate clause</li></ol>Independent clause shows the complete meaning in it. For example: Ram eats. A subordinate clause is not a complete sentence. For example: because I am sick. Sentences can also be classified on the basis of clauses.';
+    desc2.innerHTML ='<br>Classification on the basis of clauses are:<br><ol><li>A <b>simple sentence</b> consists of only one independent clause. There are no subordinate clauses.</li><li>A <b>compound sentence</b> consists of more than one independent clauses joined ny conjunctions or punctuations. There are no subordinate clauses.</li><li>A <b>complex sentence</b> consists of atleast one indpendent clause and a single subordinate clause</li><li>A <b>complex-compound sentence</b> consists of more than one independent clauses and atleast one of the independent clauses has one or more than one subordinate clauses</li></ol>';
+    /* desc3.innerHTML = '';
+    select.innerHTML = '';
+    selDesc.innerHTML = ''; */
+    desc2.style.textAlign = 'left';
+    /* desc4.innerHTML = '';
+    sentence.innerHTML = ''; */
+    desc4.style.textAlign = 'left';
+    /* reform.innerHTML = '';
+    correctness.innerHTML = '';
+    result.innerHTML = '';
+    ans = '';
+    correctAns.innerHTML = ''; */
+}
+function objective() {
+    heading.innerHTML = 'Objective';
+    desc.innerHTML ='<br><hr><br><br>The objective of this experiment is to know how to form logically correct sentences from the given words.<br>';
+    desc2.innerHTML = '<br><hr>';
+    /* desc3.innerHTML = '';
+    select.innerHTML = '';
+    selDesc.innerHTML = ''; */
+    desc2.style.textAlign = 'left';
+    /* desc4.innerHTML = ''; */
+    desc4.style.textAlign = 'left';
+    /* sentence.innerHTML = '';
+    reform.innerHTML = '';
+    correctness.innerHTML = '';
+    result.innerHTML = '';
+    ans = '';
+    correctAns.innerHTML = ''; */
+}
+
+function languageDropdown() {
+    lang = '';
+    lang = document.getElementById('language').value;
+    if (lang == 'select') {
+        ans = '';
+        correctAns.innerHTML = '';
+        clickCount = 0;
+        btnCount = 0;
+        selDesc.innerHTML = '';
+        desc3.innerHTML = '';
+        desc2.innerHTML = '';
+        desc4.innerHTML = '';
+        desc4.style.textAlign = 'left';
+        sentence.innerHTML = '';
+        reform.innerHTML = '';
+        correctness.innerHTML = '';
+        result.innerHTML = '';
+    }
+    if (lang == 'English') {
+        ans = '';
+        correctAns.innerHTML = '';
+        reform.innerHTML = '';
+        sentence.innerHTML = '';
+        correctness.innerHTML = '';
+        desc4.innerHTML = '';
+        finalSentence = '';
+        result.innerHTML = '';
+        selDesc.innerHTML ='<br><br><b>Form a sentence (Declarative or Interrogative or any other type) from the given words</b>';
+        desc3.style.color = 'blue';
+        desc3.innerHTML ='<center><i>(select the buttons in proper order)</i></center>';
+        randNo = Math.floor(Math.random() * 9);
+        var jumbledWords = engCorpus[randNo][0];
+        j = insideRandom(jumbledWords);
+        desc2.style.textAlign = 'center';
+        var b = '';
+        var bs = '';
+        clickCount = 0;
+        btnCount = 0;
+        for (i = 0; i <= j.length - 1; i++) {
+        val = j[i];
+        b ="  <button id='btn" +i +"'onclick='formSentence(this.id,this.value)' value='" +val +"'>" +val +'</button>  ';
+        bs += b;
+        btnCount++;
+        }
+        desc2.innerHTML = bs.trim();
+    }
+    if (lang == 'Hindi') {
+        ans = '';
+        correctAns.innerHTML = '';
+        reform.innerHTML = '';
+        sentence.innerHTML = '';
+        correctness.innerHTML = '';
+        desc4.innerHTML = '';
+        finalSentence = '';
+        result.innerHTML = '';
+        selDesc.innerHTML ='<br><br><b>Form a sentence (Declarative or Interrogative or any other type) from the given words</b>';
+        desc3.style.color = 'blue';
+        desc3.innerHTML ='<center><i>(select the buttons in proper order)</i></center>';
+        randNo = Math.floor(Math.random() * 6);
+        var jumbledWords = hinCorpus[randNo][0];
+        j = insiderandomizer(jumbledWords);
+        desc2.style.textAlign = 'center';
+        var b = '';
+        var bs = '';
+        btnCount = 0;
+        clickCount = 0;
+        for (i = 0; i <= j.length - 1; i++) {
+        val = j[i];
+        b ="  <button id='btn" +i +"'onclick='formSentence(this.id,this.value)' value='" +val +"'>" +val +'</button>  ';
+        bs += b;
+        btnCount++;
+        }
+        desc2.innerHTML = bs.trim();
+    }
+}
+function experiment() {
+    heading.innerHTML = '<b>Experiment</b>';
+    select.innerHTML ="<center><select id='language' onchange = 'languageDropdown()'><option value='select'>---Select Language---</option><option value='English'>English</option><option value='Hindi'>Hindi</option></select></center>";
+    // selDesc.innerHTML = '';
+    // desc3.innerHTML = '';
+    // desc.innerHTML = '';
+    // desc2.innerHTML = '';
+    // desc4.innerHTML = '';
+    desc4.style.textAlign = 'left';
+    // sentence.innerHTML = '';
+    // reform.innerHTML = '';
+    // correctness.innerHTML = '';
+    // result.innerHTML = '';
+    // ans = '';
+    // correctAns.innerHTML = '';
+}
+function quizzes() {
+    heading.innerHTML = 'Quizzes';
+    desc.innerHTML ='Which of these is a valid sentence?<ol><li>Ram came after lunch.</li><li>Ram came after having lunch.</li><li>Sleeping I saw a tiger.</li><li>I saw a sleeping tiger.</li><li>No Parking is allowed.</li></ol>';
+    desc2.innerHTML ='<br><ol><li>खाते खाते राम सो गया</li><li>राम खाते खाते सो गया</li><li>राम खाता खाते सो गया</li><li>राम खाकर सो गया</li><li>राम और श्याम ने भी खाना खाया</li><li>राम ने और श्याम भी खाना खाया</li></ol>';
+    // desc3.innerHTML = '';
+    // select.innerHTML = '';
+    // selDesc.innerHTML = '';
+    desc2.style.textAlign = 'left';
+    //desc4.innerHTML = '';
+    desc4.style.textAlign = 'left';
+    // sentence.innerHTML = '';
+    // reform.innerHTML = '';
+    // correctness.innerHTML = '';
+    // result.innerHTML = '';
+    // ans = '';
+    // correctAns.innerHTML = '';
+}
+function procedure() {
+    heading.innerHTML = 'Procedure';
+    desc.innerHTML ='<b><u>STEP 1:</u></b> Select a language which you know better<br><b><u>STEP 2:</u></b> Select the buttons which has words written on it, in a proper order<br><b><u>OUTPUT:</u></b> Group of words in a selected order will be shown<br>';
+    desc2.innerHTML ='<b><u>NOTE:</u></b><ol><li>If a wrong sentence is formed, <button>Re-form the sentence</button> is available for re-setting.</li><li>You can check whether the formed sentence is a valid or not by clicking <button>Check the correctness of this sentence</button> </li><li>For a wrong sentence, you can get the correct sentence by clicking <button>Get correct sentence</button> </li></ol>';
+    // desc3.innerHTML = '';
+    // desc3.innerHTML = '';
+    // select.innerHTML = '';
+    // selDesc.innerHTML = '';
+    desc2.style.textAlign = 'left';
+    // desc4.innerHTML = '';
+    // sentence.innerHTML = '';
+    desc4.style.textAlign = 'left';
+    // reform.innerHTML = '';
+    // correctness.innerHTML = '';
+    // result.innerHTML = '';
+    // ans = '';
+    // correctAns.innerHTML = '';
+}
